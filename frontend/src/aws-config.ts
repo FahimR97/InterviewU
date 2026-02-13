3755a6f0ad5ec24fa114aff3288244af56ebc475
FahimR97 <fahimrayhan97@gmail.com>
2026-02-13 17:43:41 +0000

    Fix GitHub Actions to deploy ServiceStack only

--- frontend/src/aws-config.ts
+++ frontend/src/aws-config.ts
@@ -1,8 +1,8 @@
 export const awsConfig = {
   Auth: {
     Cognito: {
-      userPoolId: 'eu-west-1_Y2W4Nq5cV',
-      userPoolClientId: '631upjij4alek3b8gh76474t9a',
+      userPoolId: 'eu-west-2_amQvo3MsF',
+      userPoolClientId: '12b9feb5ffl7f9pokih2lq89pq',
       loginWith: {
         email: true,
       },
@@ -11,8 +11,8 @@ export const awsConfig = {
   API: {
     REST: {
       InterviewQuestionsAPI: {
-        endpoint: 'https://kgtss21o53.execute-api.eu-west-1.amazonaws.com/prod/',
-        region: 'eu-west-1',
+        endpoint: 'https://16xcub4grb.execute-api.eu-west-2.amazonaws.com/prod/',
+        region: 'eu-west-2',
       },
     },
   },
